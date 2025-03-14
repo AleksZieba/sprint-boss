f012a775a05934c4802ac5de598a56ac40a5b9fd
AleksZieba <zeeb3d@gmail.com>
2025-03-14 19:42:28 +0100

    sprint starts properly

--- commands/interactive/handler_interactive_commands.go
+++ commands/interactive/handler_interactive_commands.go
@@ -1,13 +1,26 @@
 package interactive
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/AleksZieba/sprint-boss/internal/database"
 	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func InteractionCreate(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	dbQueries := database.New(db)
+	if err != nil {
+		log.Fatal("Failed To Load DB") //turn this into a response
+	}
 	if i.Type == discordgo.InteractionApplicationCommand {
 		switch i.ApplicationCommandData().Name {
 		case "sprint":
@@ -22,8 +35,22 @@ func InteractionCreate(s *discordgo.Session, i *discordgo.InteractionCreate) {
 					num2 = option.IntValue()
 				}
 			}
+			// func WithValue(parent Context, key, val any) Context - use with context.Background() as "Context"
+			// If the interaction occurred in a guild, use the Member.User field.
+			var userName string
+			if i.GuildID != "" && i.Member != nil {
+				userName = i.Member.User.Username
+			} else {
+				// For DM interactions, the user information is directly in i.User.
+				userName = i.User.Username
+			}
 
-			result := num1 + num2
+			err := dbQueries.StartSprint(context.WithValue(context.Background(), "username", userName)) //TODO the query function input
+			if err != nil {
+				log.Fatal("Sprint Start Failed") //turn this into a response
+			}
+
+			result := fmt.Sprintf("Sprint starts in %v minutes and will last %v minutes", num1, num2)
 			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
 				Data: &discordgo.InteractionResponseData{
