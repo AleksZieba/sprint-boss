0af19fe76ebda4050d16d39f55d41d72579ff390
AleksZieba <zeeb3d@gmail.com>
2025-03-14 13:45:16 +0100

    ready command anded

--- commands/interactive/handler_interactive_commands.go
+++ commands/interactive/handler_interactive_commands.go
@@ -10,14 +10,34 @@ func InteractionCreate(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	if i.Type == discordgo.InteractionApplicationCommand {
 		switch i.ApplicationCommandData().Name {
 		case "sprint":
-			var num1 int64 = 10 // default value if not provided
+			var num1 int64 = 0  // default value if not provided
 			var num2 int64 = 20 // default value if not provided
 
 			for _, option := range i.ApplicationCommandData().Options {
 				switch option.Name {
-				case "startdelay":
+				case "start_delay_minutes":
 					num1 = option.IntValue()
-				case "totaltime":
+				case "sprint_time":
+					num2 = option.IntValue()
+				}
+			}
+
+			result := num1 + num2
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: fmt.Sprintf("The sum is: %d", result),
+				},
+			})
+		case "ready":
+			var num1 int64 = 0  // default value if not provided
+			var num2 int64 = 20 // default value if not provided
+
+			for _, option := range i.ApplicationCommandData().Options {
+				switch option.Name {
+				case "start_delay_minutes":
+					num1 = option.IntValue()
+				case "sprint_time":
 					num2 = option.IntValue()
 				}
 			}
--- commands/interactive/interactive_commands.go
+++ commands/interactive/interactive_commands.go
@@ -9,17 +9,34 @@ var Commands = []*discordgo.ApplicationCommand{
 		Options: []*discordgo.ApplicationCommandOption{
 			{
 				Type:        discordgo.ApplicationCommandOptionInteger,
-				Name:        "startdelay",
+				Name:        "start_delay_minutes",
 				Description: "number of minutes till the sprint starts",
 				Required:    false,
 			},
 			{
 				Type:        discordgo.ApplicationCommandOptionInteger,
-				Name:        "totaltime",
+				Name:        "sprint_time",
 				Description: "when your sprint starts and how long it is",
 				Required:    false,
 			},
 		},
 	},
-	//{}
+	{
+		Name:        "ready",
+		Description: "Start a sprint",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionInteger,
+				Name:        "start_delay_minutes",
+				Description: "number of minutes till the sprint starts",
+				Required:    false,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionInteger,
+				Name:        "sprint_time",
+				Description: "when your sprint starts and how long it is",
+				Required:    false,
+			},
+		},
+	},
 }
